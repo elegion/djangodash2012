6dd4eb21f6598bbaad329645a3965ad9d47c41db
Dmitry Medvinsky <dmedvinsky@gmail.com>
2012-08-18 15:16:31 +0400

    Add test runner task stub

--- fortuitus/frunner/tasks.py
+++ fortuitus/frunner/tasks.py
@@ -5,3 +5,16 @@ from celery import task
 def add(x, y):
     """ Test task. """
     return x + y
+
+
+@task()
+def run_tests(test_id):
+    """
+    A task that actually runs the API testing.
+
+    First it copies the test data to the run history tables, then runs the
+    tests.
+
+    """
+    # TODO
+    pass
