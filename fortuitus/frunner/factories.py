3dd8db296d7db01f75ab9cda952cf6bf0664e4f4
vladimir.lyukov <v.lyukov@gmail.com>
2012-08-18 19:33:07 +0400

    Fixed factories
    Use factory.SubFactory instead of factory.LazyAttribute

--- fortuitus/frunner/factories.py
+++ fortuitus/frunner/factories.py
@@ -7,20 +7,23 @@ from fortuitus.frunner import models
 class TestCaseF(factory.Factory):
     FACTORY_FOR = models.TestCase
 
-    project = factory.LazyAttribute(lambda o: TestProjectF.create())
+    project = factory.SubFactory(TestProjectF)
     name = factory.Sequence(lambda n: 'TestCase #%s' % n)
 
 
 class TestCaseStepF(factory.Factory):
     FACTORY_FOR = models.TestCaseStep
 
+    testcase = factory.SubFactory(TestCaseF)
     order = 1
     method = models.models_base.Method.GET
     url = 'user_list.json'
 
+
 class TestCaseAssertF(factory.Factory):
     FACTORY_FOR = models.TestCaseAssert
 
+    step = factory.SubFactory(TestCaseStepF)
     order = 1
     lhs = ''
     rhs = ''
