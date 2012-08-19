f32d53e696ed0916921367c417c758ef8a621402
Dmitry Medvinsky <dmedvinsky@gmail.com>
2012-08-19 16:47:20 +0400

    Code style

--- fortuitus/feditor/urls.py
+++ fortuitus/feditor/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('fortuitus.feditor.views',
-    url(r'^(?P<company>[\w\d-]+)/(?P<project>[\w\d-]+)/$', 'project', name='feditor_project'),
+    url(r'^(?P<company>[\w\d-]+)/(?P<project>[\w\d-]+)/$',
+        'project', name='feditor_project'),
 )
