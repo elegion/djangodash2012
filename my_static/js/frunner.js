04f2a622531deed671206615aa6f9ad732c79e4b
vladimir.lyukov <v.lyukov@gmail.com>
2012-08-19 23:37:15 +0400

    Added syntax highlighting for responses

--- my_static/js/frunner.js
+++ my_static/js/frunner.js
@@ -1,3 +1,5 @@
+"use strict";
+
 Fortuitus.frunner = {
   initTestrun: function() {
     $('.js_toggle_request').click(function() {
@@ -8,5 +10,16 @@ Fortuitus.frunner = {
       $(this).parents('li').find('.js_response_wrapper').slideToggle();
       return false;
     });
+
+    $('pre code').each(function(i, e) {
+      var $e = $(e),
+          language = hljs.highlightAuto($e.text()).language;
+      if (language == 'json') {
+        $e.text(vkbeautify.json($e.text()));
+      } else if (language == 'xml' || language == 'html') {
+        $e.text(vkbeautify.xml($e.text()));
+      }
+      hljs.highlightBlock(e);
+    });
   }
-}
+};
