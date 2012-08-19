234367728ad9d9dc91a121425c8caa2650478e41
Dmitry Medvinsky <dmedvinsky@gmail.com>
2012-08-19 16:39:42 +0400

    Add doc

--- fortuitus/feditor/forms.py
+++ fortuitus/feditor/forms.py
@@ -4,6 +4,7 @@ from fortuitus.feditor.models import TestCase
 
 
 class TestCaseForm(forms.ModelForm):
+    """ ModelForm for TestCase model. """
     class Meta:
         model = TestCase
         exclude = ('project',)
